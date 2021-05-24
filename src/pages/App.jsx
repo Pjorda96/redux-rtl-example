eeb64d3e442d07ded7093652fa9abaa71816eb95
Pablo Jorda <pjorda96@gmail.com>
2021-05-24 12:18:47 +0200

    atomized project

---
+++ src/pages/App.jsx
@@ -0,0 +1,45 @@
+import React, { lazy, Suspense } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Spinner from "../components/Spinner";
+const Navbar = lazy(() => import('../components/Navbar'));
+
+const About = lazy(() => import('./About'));
+const Users = lazy(() => import('./Users'));
+const Home = lazy(() => import('./Home'));
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Suspense fallback={<Spinner />}>
+          <Navbar />
+        </Suspense>
+
+        <Switch>
+          <Route path="/about">
+            <Suspense fallback={<Spinner />}>
+              <About />
+            </Suspense>
+          </Route>
+
+          <Route path="/users">
+            <Suspense fallback={<Spinner />}>
+              <Users />
+            </Suspense>
+          </Route>
+
+          <Route path="/">
+            <Suspense fallback={<Spinner />}>
+              <Home />
+            </Suspense>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
